329714e9aab4062c579611302cc6d32b02658acb
rdpupptime <74958882+rdpupptime@users.noreply.github.com>
2020-12-29 01:10:01 -0600

    Update miner.py

--- simpleCoin/miner.py
+++ simpleCoin/miner.py
@@ -252,7 +252,7 @@ def welcome_msg():
     print("""       =========================================\n
         SIMPLE COIN v1.0.0 - BLOCKCHAIN SYSTEM\n
        =========================================\n\n
-        You can find more help at: https://github.com/cosme12/SimpleCoin\n
+        You can find more help at: https://github.com/rdpupptime/SimpleCoin\n
         Make sure you are using the latest version or you may end in
         a parallel chain.\n\n\n""")
 
